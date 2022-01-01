58c0c2b637ed40a30840d7a95b2c28127c895177
zjkjzj <wy163zhuj@163.com>
2022-01-01 09:16:47 +0800

    fix(ofd): fix train usage

--- ofd/criterion/build.py
+++ ofd/criterion/build.py
@@ -9,7 +9,7 @@
 
 from zcls.model import registry
 from zcls.model.criterions.crossentropy_loss import CrossEntropyLoss
-from zcls.model.criterions.label_smoothing_loss import LabelSmoothingLoss
+from zcls.model.criterions.label_smoothing_loss import get_label_entropy_loss
 
 from .ofd_loss import OFDLoss
 
--- ofd/engine/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@date: 2021/9/1 下午9:10
-@file: __init__.py.py
-@author: zj
-@description: 
-"""
